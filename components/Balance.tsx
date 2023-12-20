867f2f653e5d8abdfdf8aba429e5f842c36b7310
miralsuthar <miralsuthar2000@gmail.com>
2023-12-20 10:43:33 +0530

    feat: add useBalance hook

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,26 +1,17 @@
 import { RefreshCcw } from "lucide-react";
 import { Button } from "./ui/button";
-import { WalletUnlocked } from "fuels";
-import { useEffect, useState } from "react";
 
-export const Balance = ({ wallet }: { wallet: WalletUnlocked }) => {
-  const [balance, setBalance] = useState<number>();
-  const [isBalanceCheck, setIsBalanceCheck] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      if (isBalanceCheck) {
-        const balance = await wallet?.getBalance();
-        setBalance(balance?.toNumber()!);
-        setIsBalanceCheck(false);
-      }
-    })();
-  }, [wallet, isBalanceCheck]);
-
+export const Balance = ({
+  balance,
+  refetch,
+}: {
+  balance: number;
+  refetch: () => void;
+}) => {
   return (
     <div className="flex justify-center items-center gap-3">
-      <span>{balance ? balance : 0} ETH</span>
-      <Button onClick={() => setIsBalanceCheck(true)}>
+      <span>{balance} ETH</span>
+      <Button onClick={refetch}>
         <RefreshCcw size={16} />
       </Button>
     </div>
--- hooks/useBalance.ts
+++ hooks/useBalance.ts
@@ -1,13 +1,24 @@
-import { Provider } from "fuels";
-import { useEffect, useState } from "react";
+import { type AbstractAddress, Provider, BaseAssetId, Address } from "fuels";
+import { useEffect, useMemo, useState } from "react";
 
-export const useBalance = (provider: Provider) => {
-  const [balance, setBalance] = useState<number>();
-  const [blockNumber, setBlockNumber] = useState<string>("");
+export const useBalance = (address: `fuel${string}`, provider: Provider) => {
+  const [balance, setBalance] = useState<number>(0);
+  const [isRefetching, setIsRefetching] = useState(true);
 
-  provider?.getBlockNumber().then((value) => setBlockNumber(value.toString()));
+  const refetch = () => {
+    setIsRefetching(true);
+  };
 
   useEffect(() => {
-    console.log("this is running");
-  }, [blockNumber]);
+    if (provider && address && isRefetching) {
+      const ownerAddress = new Address(address);
+      (async () => {
+        const balance = await provider.getBalance(ownerAddress, BaseAssetId);
+        setBalance(balance?.toNumber());
+      })();
+      setIsRefetching(false);
+    }
+  }, [address, provider, isRefetching]);
+
+  return { balance, refetch };
 };
