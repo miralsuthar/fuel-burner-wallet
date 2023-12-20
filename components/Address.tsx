b1ef1c2535d732ae2bdd45ead09d71486cc9f0cc
miralsuthar <miralsuthar2000@gmail.com>
2023-12-20 10:44:46 +0530

    refactor: remove unwanted code and make it more readable

--- components/Address.tsx
+++ components/Address.tsx
@@ -1,21 +1,27 @@
-import { WalletUnlocked } from "fuels";
 import { Copy } from "lucide-react";
+import { useEffect, useState } from "react";
 
-export const Address = ({ wallet }: { wallet: WalletUnlocked }) => {
+export const Address = ({ address }: { address: string }) => {
   const copyAddress = async () => {
-    await navigator.clipboard.writeText(
-      wallet.address.toAddress().substring(4)
-    );
+    await navigator.clipboard.writeText(address?.substring(4));
   };
 
-  const address = wallet.address.toString();
+  const [isLoad, setIsLoad] = useState(false);
+
+  useEffect(() => {
+    if (window !== undefined) {
+      setIsLoad(true);
+    }
+  }, []);
 
   return (
     <div className="flex justify-center items-center gap-4">
-      <span>
-        {address?.substring(0, 6)}....
-        {address?.substring(address.length - 5)}
-      </span>
+      {isLoad && (
+        <p>
+          <span>{address?.substring(0, 6)}....</span>
+          <span>{address?.substring(address.length - 5)}</span>
+        </p>
+      )}
       <Copy className="cursor-pointer" size={10} onClick={copyAddress} />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import { generateWallet } from "@/utils";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { FuelContext } from "./_app";
 import { BaseAssetId, WalletUnlocked, Address as TrasnferAddress } from "fuels";
 import { QRCodeSVG } from "qrcode.react";
@@ -9,21 +9,46 @@ import { Balance } from "@/components/Balance";
 import { Address } from "@/components/Address";
 import { Input } from "@/components/ui/input";
 import QrReader from "react-qr-reader-es6";
+import { useLocalStorage } from "usehooks-ts";
+import { useBalance } from "@/hooks/useBalance";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// type wallet = {
+//   address: string;
+//   provider: {
+//     operations: {};
+//     options: {};
+//     url: string;
+//   };
+// };
+
 export default function Home() {
   const { provider } = useContext(FuelContext);
-  const [wallet, setWallet] = useState<WalletUnlocked>();
+  const [wallet, setWallet] = useLocalStorage("wallet", {} as WalletUnlocked);
+  const [walletPrivateKey, setWalletPrivateKey] = useLocalStorage(
+    "walletPrivateKey",
+    ""
+  );
 
   const [address, setAddress] = useState<string>();
   const [amount, setAmount] = useState<any>();
 
+  const { balance, refetch } = useBalance(
+    wallet?.address?.toString() as `fuel${string}`,
+    provider!
+  );
+
   const [isScanning, setIsScanning] = useState(false);
 
   const transfer = async () => {
-    const transferAddress = new TrasnferAddress(`fuel${address}`);
-    await wallet?.transfer(transferAddress, amount, BaseAssetId, {
+    const transferAddress = new TrasnferAddress(address as `fuel${string}`);
+    // await wallet?.transfer(transferAddress, amount, BaseAssetId, {
+    //   gasLimit: 10_000,
+    //   gasPrice: 10_000,
+    // });
+    const unlockedWallet = new WalletUnlocked(walletPrivateKey, provider!);
+    await unlockedWallet.transfer(transferAddress, amount, BaseAssetId, {
       gasLimit: 10_000,
       gasPrice: 10_000,
     });
@@ -33,54 +58,58 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-center gap-10 ${inter.className}`}
     >
-      {wallet && <Address wallet={wallet} />}
-      {wallet && <Balance wallet={wallet} />}
-      {wallet && (
-        <QRCodeSVG
-          className="w-9/12 h-80"
-          value={wallet?.address.toAddress().substring(4)}
-        />
-      )}
-      <Button
-        onClick={() => {
-          const wallet = generateWallet(provider!);
-          setWallet(wallet);
-          window.localStorage.setItem("wallet", JSON.stringify(wallet));
-        }}
-        className="btn"
-      >
-        Generate
-      </Button>
-      {wallet && isScanning && (
-        <QrReader
-          delay={300}
-          onError={(e) => {
-            console.log(e);
-          }}
-          onScan={(e) => {
-            if (e !== null) {
-              setAddress(e);
-              setIsScanning(false);
-            }
-          }}
-          style={{ width: "60%" }}
-        />
-      )}
-      {wallet && <Button onClick={() => setIsScanning(true)}>Scan</Button>}
-      {wallet && (
-        <div className="flex flex-col gap-4">
-          <Input
-            placeholder="address"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
+      {wallet ? (
+        <>
+          <Address address={wallet.address?.toString()} />
+          <Balance balance={balance} refetch={() => refetch()} />
+          <QRCodeSVG
+            className="w-9/12 h-80"
+            value={wallet?.address?.toString()}
           />
-          <Input
-            placeholder="amount"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-          <Button onClick={transfer}>Trasnfer</Button>
-        </div>
+
+          {isScanning && (
+            <div className="absolute top-0 left-0">
+              <QrReader
+                delay={300}
+                onError={(e) => {
+                  console.log(e);
+                }}
+                onScan={(e) => {
+                  if (e !== null) {
+                    setAddress(e);
+                    setIsScanning(false);
+                  }
+                }}
+                style={{ width: "60%" }}
+              />
+            </div>
+          )}
+          <Button onClick={() => setIsScanning(true)}>Scan</Button>
+          <div className="flex flex-col gap-4">
+            <Input
+              placeholder="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            <Input
+              placeholder="amount"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+            <Button onClick={transfer}>Trasnfer</Button>
+          </div>
+        </>
+      ) : (
+        <Button
+          onClick={() => {
+            const wallet = generateWallet(provider!);
+            setWalletPrivateKey(wallet.privateKey);
+            setWallet(wallet);
+          }}
+          className="btn"
+        >
+          Generate
+        </Button>
       )}
     </main>
   );
