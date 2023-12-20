92bd7d3aa41de6e983ad6a635e94a53dc867eb00
miralsuthar <miralsuthar2000@gmail.com>
2023-12-20 11:49:43 +0530

    feat: create useTranfer hook for transfering assets

--- hooks/useTransfer.ts
+++ hooks/useTransfer.ts
@@ -1,14 +1,41 @@
-import { AbstractAddress, BigNumberish, WalletUnlocked } from "fuels";
+import {
+  AbstractAddress,
+  BigNumberish,
+  WalletUnlocked,
+  Address as TransferAddress,
+  Provider,
+  BaseAssetId,
+} from "fuels";
 import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
 
 export const useTransfer = (
-  wallet: WalletUnlocked,
-  address: AbstractAddress,
-  amount: BigNumberish
+  address: `fuel${string}`,
+  amount: BigNumberish,
+  walletPrivateKey: string,
+  provider: Provider
 ) => {
+  const { toast } = useToast();
+
   const transfer = async () => {
-    await wallet.transfer(address, amount);
+    const transferAddress = new TransferAddress(address as `fuel${string}`);
+    const unlockedWallet = new WalletUnlocked(walletPrivateKey, provider!);
+    try {
+      await unlockedWallet.transfer(transferAddress, amount, BaseAssetId, {
+        gasLimit: 10_000,
+        gasPrice: 10_000,
+      });
+      toast({
+        title: "ETHER Transfer successfully",
+        variant: "default",
+      });
+    } catch (error) {
+      toast({
+        title: "Error transfering ETHER",
+        variant: "destructive",
+      });
+    }
   };
 
-  return transfer;
+  return { transfer };
 };
